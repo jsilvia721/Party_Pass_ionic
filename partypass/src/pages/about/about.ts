2f5c8b82f1a68799e382ce463fc9f6ecdfe942aa
Joshua Silvia <jsilvia@umass.edu>
2017-04-23 15:10:57 -0400

    Google maps view added to map tab

--- partypass/src/pages/about/about.ts
+++ partypass/src/pages/about/about.ts
@@ -1,14 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import {
- GoogleMaps,
- GoogleMap,
- GoogleMapsEvent,
- LatLng,
- CameraPosition,
- MarkerOptions,
- Marker
-} from '@ionic-native/google-maps';
+
 
 /**
  * Generated class for the Map page.
@@ -16,58 +8,36 @@ import {
  * See http://ionicframework.com/docs/components/#navigation for more info
  * on Ionic pages and navigation.
  */
+ declare var google;
 
 @Component({
-  selector: 'page-about',
+  selector: 'about-page',
   templateUrl: 'about.html'
 })
 export class AboutPage {
- constructor(private googleMaps: GoogleMaps) {}
-
-// Load map only after view is initialized
-ngAfterViewInit() {
- this.loadMap();
-}
-
-loadMap() {
- // make sure to create following structure in your view.html file
- // and add a height (for example 100%) to it, else the map won't be visible
- // <ion-content>
- //  <div #map id="map" style="height:100%;"></div>
- // </ion-content>
-
- // create a new map by passing HTMLElement
- let element: HTMLElement = document.getElementById('map');
-
- let map: GoogleMap = this.googleMaps.create(element);
-
- // listen to MAP_READY event
- // You must wait for this event to fire before adding something to the map or modifying it in anyway
- map.one(GoogleMapsEvent.MAP_READY).then(() => console.log('Map is ready!'));
-
- // create LatLng object
- let ionic: LatLng = new LatLng(43.0741904,-89.3809802);
-
- // create CameraPosition
- let position: CameraPosition = {
-   target: ionic,
-   zoom: 18,
-   tilt: 30
- };
-
- // move the map's camera to position
- map.moveCamera(position);
-
- // create new marker
- let markerOptions: MarkerOptions = {
-   position: ionic,
-   title: 'Ionic'
- };
-
- // const marker: Marker = map.addMarker(markerOptions)
- //   .then((marker: Marker) => {
- //      marker.showInfoWindow();
- //    });
- }
-
+ 
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+ 
+  constructor(public navCtrl: NavController) {
+ 
+  }
+ 
+  ionViewDidLoad(){
+    this.loadMap();
+  }
+ 
+  loadMap(){
+ 
+    let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+ 
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+ 
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+ 
+  }
 }
