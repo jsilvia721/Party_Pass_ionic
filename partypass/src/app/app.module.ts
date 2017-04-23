628b0b878634409046801630d178053f2fc4172b
Joshua Silvia <jsilvia@umass.edu>
2017-04-22 21:25:55 -0400

    added map tab on adminView

--- partypass/src/app/app.module.ts
+++ partypass/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { Modal } from '../pages/modal/modal';
@@ -17,7 +16,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
     TabsPage,
     Modal,
@@ -31,7 +29,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
     TabsPage,
     Modal,
