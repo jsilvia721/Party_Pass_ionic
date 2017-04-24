e717bea8e64b25c874eb51fce08e57c97b770bd0
Roman Ganchin <rganchin@umass.edu>
2017-04-24 15:00:46 -0400

    add marker functionality

--- partypass/src/pages/about/about.ts
+++ partypass/src/pages/about/about.ts
@@ -3,41 +3,69 @@ import { NavController } from 'ionic-angular';
 
 
 /**
- * Generated class for the Map page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
- declare var google;
+* Generated class for the Map page.
+*
+* See http://ionicframework.com/docs/components/#navigation for more info
+* on Ionic pages and navigation.
+*/
+declare var google;
 
 @Component({
   selector: 'about-page',
   templateUrl: 'about.html'
 })
 export class AboutPage {
- 
+
   @ViewChild('map') mapElement: ElementRef;
   map: any;
- 
+
   constructor(public navCtrl: NavController) {
- 
+
   }
- 
+
   ionViewDidLoad(){
     this.loadMap();
   }
- 
+
   loadMap(){
- 
+
     let latLng = new google.maps.LatLng(42.3912, -72.5267);
- 
+
     let mapOptions = {
       center: latLng,
       zoom: 13,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     }
- 
+
     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
- 
+
   }
+
+  addInfoWindow(marker, content){
+
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+
+  }
+
+  addMarker(){
+
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+
+    let content = "<h4>Information!</h4>";
+
+    this.addInfoWindow(marker, content);
+
+  }
+
+
 }
