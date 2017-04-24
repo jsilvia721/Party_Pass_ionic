a1a049dbb58c7da44b1786958c9a877583490a9e
Roman Ganchin <rganchin@umass.edu>
2017-04-24 16:40:43 -0400

    we know have coordinates

--- partypass/src/pages/autocomplete/autocomplete.ts
+++ partypass/src/pages/autocomplete/autocomplete.ts
@@ -1,6 +1,5 @@
 import { Component, NgZone } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
-
 /**
  * Generated class for the Autocomplete page.
  *
@@ -19,7 +18,7 @@ export class Autocomplete {
   autocompleteItems;
   autocomplete;
   service = new google.maps.places.AutocompleteService();
-
+  geocoder = new google.maps.Geocoder();
   constructor (public viewCtrl: ViewController, private zone: NgZone) {
     this.autocompleteItems = [];
     this.autocomplete = {
@@ -43,15 +42,31 @@ export class Autocomplete {
     let me = this;
     this.service.getPlacePredictions({ input: this.autocomplete.query }, function (predictions, status) {
       me.autocompleteItems = [];
+      console.log(predictions[0].description);
+      //console.log(me);
+      me.geocoder.geocode({'address': predictions[0].description}, function(results, status) {
+          if (status === 'OK') {
+            console.log(results[0].geometry.location.lat());
+            console.log(results[0].geometry.location.lng());
+            // var marker = new google.maps.Marker({
+            //   map: resultsMap,
+            //   position: results[0].geometry.location
+            // });
+          } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+          }
+        });
+
+      // me.geocoder.forwardGeocode({input: })
+  //     me.nativeGeocoder.forwardGeocode("Berlin")
+  // .then((coordinates: NativeGeocoderForwardResult) => console.log("The coordinates are latitude=" + coordinates.latitude + " and longitude=" + coordinates.longitude))
+  // .catch((error: any) => console.log(error));
       me.zone.run(function () {
         if (predictions){
           predictions.forEach(function (prediction) {
             me.autocompleteItems.push(prediction.description);
           });
         }
-        //  else {
-        //   me.autocompleteItems.push("No addresses available");
-        // }
       });
     });
   }
