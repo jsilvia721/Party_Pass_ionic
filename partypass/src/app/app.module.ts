afa14c01c4fc15ed7ff3b6baec03084342351a6f
Nick Merlino <nmerlino@umass.edu>
2017-04-24 22:25:46 -0400

    Removed unused imports, reducing warnings and making it faster

--- partypass/src/app/app.module.ts
+++ partypass/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-
 import { AboutPage } from '../pages/about/about';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
@@ -11,17 +10,8 @@ import { Registration } from '../pages/registration/registration';
 import { Edit } from '../pages/edit/edit'
 import { Parties } from '../providers/parties'
 import { Autocomplete } from '../pages/autocomplete/autocomplete';
-import { Http, Headers, HttpModule } from '@angular/http';
-import {
- GoogleMaps,
- GoogleMap,
- GoogleMapsEvent,
- LatLng,
- CameraPosition,
- MarkerOptions,
- Marker
-} from '@ionic-native/google-maps';
-
+import { HttpModule } from '@angular/http';
+import { GoogleMaps } from '@ionic-native/google-maps';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
